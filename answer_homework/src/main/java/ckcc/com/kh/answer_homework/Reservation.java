d6616f75727d0731761b25c40c051a364a9cec66
noemphearak <phearakkosign@gmail.com>
2018-06-09 17:48:30 +0700

    Udpated Reservation

--- answer_homework/src/main/java/ckcc/com/kh/answer_homework/Reservation.java
+++ answer_homework/src/main/java/ckcc/com/kh/answer_homework/Reservation.java
@@ -1,5 +1,20 @@
 package ckcc.com.kh.answer_homework;
 
 public class Reservation {
-
+	private User madeBy = new User();
+	
+	// TODO get name user that made reservation
+	public User getMadeBy() {
+		return madeBy;
+	}
+	
+	// TODO set name user who made reservation
+	public void setMadeBy(User user) {
+		this.madeBy = user;
+	}
+	
+	// TODO method for get name who cancel 
+	public User canBeCancelledBy(boolean user) {
+		return madeBy;
+	}
 }
