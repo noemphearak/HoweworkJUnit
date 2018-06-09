a25619d3e33c54eaaded7e0aa8a98b6ab074d013
noemphearak <phearakkosign@gmail.com>
2018-06-09 17:39:55 +0700

    Updated

--- answer_homework/src/main/java/ckcc/com/kh/answer_homework/User.java
+++ answer_homework/src/main/java/ckcc/com/kh/answer_homework/User.java
@@ -1,5 +1,15 @@
 package ckcc.com.kh.answer_homework;
 
 public class User {
-
+	private boolean isAdmin;
+	
+	// TODO get admin
+	public boolean getAdmin() {
+		return isAdmin;
+	}
+	
+	// TODO set admin 
+	public void setAdmin(boolean isAdmin) {
+		this.isAdmin = isAdmin;
+	}
 }
